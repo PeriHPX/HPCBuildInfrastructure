43149be11422b5e442113938406da9d58ebc4005
Patrick Diehl <diehlpk@users.noreply.github.com>
2023-10-19 21:21:39 -0500

    Update config.sh to the latest version

--- config.sh
+++ config.sh
@@ -16,13 +16,13 @@ export BLAZE_ITERATIVE_VERSION=master
 export YAMLCPP_VERSION=0.6.3
 
 # CMAKE
-export CMAKE_VERSION=3.21.0
+export CMAKE_VERSION=3.24.0
 
 # HWLOC
 export HWLOC_VERSION=2.2.0
 
 # BOOST 
-export BOOST_VERSION=1.78.0
+export BOOST_VERSION=1.80.0
 export BOOST_ROOT=${INSTALL_ROOT}/boost
 export BOOST_BUILD_TYPE=$(echo ${BUILD_TYPE/%WithDebInfo/ease} | tr '[:upper:]' '[:lower:]')
 
@@ -30,10 +30,10 @@ export BOOST_BUILD_TYPE=$(echo ${BUILD_TYPE/%WithDebInfo/ease} | tr '[:upper:]' 
 export JEMALLOC_VERSION=5.2.0
 
 # GCC
-export GCC_VERSION=9.3.0
+export GCC_VERSION=13.0
 
 # HPX
-export HPX_VERSION=1.7.1
+export HPX_VERSION=1.9.1
 
 # VTK 
 export VTK_VERSION=9.1.0
